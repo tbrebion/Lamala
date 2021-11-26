9f5125cedd87aa2384cf80e5df1191f6e30a070c
Titouan BREBION <tbrebion@e3r11p19.clusters.42paris.fr>
2021-11-26 16:23:31 +0100

    libft.h_update

--- libft/header/libft.h
+++ libft/header/libft.h
@@ -6,7 +6,7 @@
 /*   By: tbrebion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 18:47:38 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/11/24 15:54:53 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/11/26 16:22:46 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,25 @@ int     ft_memcmp(const void *s1, const void *s2, size_t n);
 void    *ft_memcpy(void *dst, const void *scr, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memset(void *ptr, int c, size_t len);
-char    *strchr(const char *str, int c);
+char    *ft_strchr(const char *str, int c);
 char    *ft_strdup(const char *str);
 char    *ft_strjoin(const char *s1, const char *s2);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t  strlcpy(char *dst, const char *src, size_t dstsize);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlen(const char *str);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 char    *ft_strnstr(const char *str, const char *to_find, size_t n);
-char    *strrchr(const char *str, int c);
+char    *ft_strrchr(const char *str, int c);
 char    *ft_strtrim(char const *s1, char const *set);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 int     ft_tolower(int c);
 int     ft_toupper(int c);
-
+char    **ft_split(char const *s, char c);
+char    *ft_itoa(int n);
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void    ft_striteri(char *s, void (*f)(unsigned int, char*));
+void    ft_putchar_fd(char c, int fd);
+void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
 
 #endif
