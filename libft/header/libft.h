6ddd590cf719e0770f5d758f2f861349b2032c53
tbrebion <titouan.brebion@yahoo.com>
2021-11-28 17:13:17 +0100

    struct

--- libft/header/libft.h
+++ libft/header/libft.h
@@ -16,6 +16,12 @@
 # include<stdlib.h>
 # include<unistd.h>
 
+typedef struct	s_list
+{
+	void		*content;
+	struct s_list	*next;
+}			t_list;
+
 int     ft_atoi(const char *str);
 void    ft_bzero(void *s, size_t n);
 void    *ft_calloc(size_t n, size_t size);
@@ -50,5 +56,8 @@ void    ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
+
+t_list *ft_lstnew(void *content);
+
 
 #endif
