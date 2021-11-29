2e96f7a1ac1e37860ad21913934219e96356c0a5
Titouan BREBION <tbrebion@e3r3p19.clusters.42paris.fr>
2021-11-29 15:55:33 +0100

    bonus

--- libft.a/libc_bonus/ft_lstadd_front.c
+++ libft.a/libc_bonus/ft_lstadd_front.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/29 14:47:50 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/11/29 14:55:28 by tbrebion         ###   ########.fr       */
+/*   Created: 2021/11/29 14:56:58 by tbrebion          #+#    #+#             */
+/*   Updated: 2021/11/29 15:33:14 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/libft.h"
 
-t_list	*ft_lstnew(void *content)
+void	ft_lstadd_front(t_list **alst, t_list *new)
 {
-	t_list	*ptr;
-
-	ptr = (t_list *)malloc(sizeof(*ptr));
-	if (ptr == NULL)
-		return (NULL);
-	ptr->content = content;
-	ptr->next = NULL;
-	return (ptr);
+	if (alst)
+	{
+		if (*alst)
+			new->next = *alst;
+		*alst = new;
+	}
 }
