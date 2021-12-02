410aa1d59b6d876475783514efa24d2881c8bec8
Titouan BREBION <tbrebion@e3r13p8.clusters.42paris.fr>
2021-12-02 15:25:38 +0100

    t

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:08:09 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/11/24 15:37:23 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/12/02 15:17:45 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
+	while (*s1 != '\0' && *s2 != '\0' && n > 0)
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-			return (s1[i] - s2[i]);
+		if (*s1 != *s2)
+			break ;
+		s1++;
+		s2++;
+		n--;
 	}
-	return (0);
+	if (n == 0)
+		return (0);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
