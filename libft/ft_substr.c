98ffc158c23778fe71b3909aad1a8ccbfa564143
Titouan BREBION <tbrebion@e1r4p16.42.fr>
2021-12-02 17:57:16 +0100

    test

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:44:30 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/11/24 14:53:51 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/12/02 17:00:24 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	res = malloc(sizeof(char) * (len + 1));
 	if (res == NULL)
 		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		res[i] = '\0';
+		return (res);
+	}
 	while (i < start)
 		i++;
 	while (s[i] && j < len)
