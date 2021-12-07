3c5b553f58983320ce154da1dda743ba9a2376bf
Titouan BREBION <tbrebion@e3r8p1.42.fr>
2021-12-07 17:38:03 +0100

    ok

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 14:56:58 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/11/29 16:37:28 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/12/07 17:34:17 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ void	ft_lstadd_front(t_list **alst, t_list *new)
 		new->next = *alst;
 		*alst = new;
 	}
+	*alst = new;
 }
