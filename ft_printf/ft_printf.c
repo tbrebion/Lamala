1b2e41af02a73629ef65ed17bf4deb915d4d0b7b
Titouan BREBION <tbrebion@e3r2p23.clusters.42paris.fr>
2021-12-09 15:27:36 +0100

    ok

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 18:20:41 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/12/09 13:27:28 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/12/09 15:27:23 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,19 @@ int	char_counter(const char *save, va_list args)
 			i++;
 			len++;
 		}
-		if (save[i + 1] && is_in_typelist(save[i + 1] == 1))
+		if (save[i + 1] && is_in_typelist(save[i + 1]) == 1)
 		{
-
+			i++;
+			if (save[i] == 'c' || save[i] == '%')
+				len++;
+			if (save[i] == 's' || save[i] == 'x'|| save[i] == 'X' || save[i] == 'p')
+				len += ft_strlen(args);
+			if (save[i] == 'i' || save[i] == 'd' || save[i] == 'u')
+				len += nlen(args);
+			i++;
 		}
+		i++;
+		len++;
 	}
 	return (len);
 }
@@ -36,42 +45,62 @@ int	ft_printf(const char *input, ...)
 {
 	int		i;
 	int		char_count;
-	const char	*save;
 	va_list		args;
 
 	va_start(args, input);
 	i = 0;
-	char_count = 0;
-	save = ft_strdup(input);
+	char_count = char_counter(input, args);
 	while (input[i])
 	{
 		while (input[i] != '%')
 		{
 			ft_putchar(input[i]);
 			i++;
-			if (input[i] == '\0')
-				return (ft_strlen(input));
 		}
 		if (input[i] == '%')
 		{
 			if (input[i + 1] == 'c')
+			{
 				ft_putchar(va_arg(args, int));
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == 's')
+			{
 				ft_putstr(va);
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == 'p')
+			{
 
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == 'd')
+			{
 				ft_putnbr(nb);
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == 'i')
+			{
 				ft_putnbr(nb);
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == 'u')
-
+			{
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == 'x')
-
+			{
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == 'X')
-
+			{
+				char_count += char_counter(input, args);
+			}
 			if (input[i + 1] == '%')
+			{
 				write (1, "%", 1);
+				char_count++;
+			}
 			i++;	
 		}
 		i++;
