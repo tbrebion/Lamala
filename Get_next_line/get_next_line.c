3d000fc214e06edb3a41c70569088ee8bb7a47f0
Titouan BREBION <tbrebion@e3r12p9.42.fr>
2021-12-17 17:05:16 +0100

    GNL

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -1,2 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbrebion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 12:42:30 by tbrebion          #+#    #+#             */
+/*   Updated: 2021/12/17 17:02:06 by tbrebion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*get_line(char *str)
+{
+	int	i;
+	char	*res;
+
+	i = 0;
+	if (!str[i])
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	res = malloc(sizeof(char) * i + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		res[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		res[i] == str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buff;
+	char		*res;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	return (res);
+}
--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -1,10 +1,22 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbrebion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 17:02:38 by tbrebion          #+#    #+#             */
+/*   Updated: 2021/12/17 17:04:29 by tbrebion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include<unistd.h>
+# include<stdlib.h>
 
-
+char	*get_next_line(int fd);
+char	*get_line(char *str);
 
 # endif
