0821869bdb03b21579ff566026777c1ea06dd466
Titouan BREBION <tbrebion@e3r12p9.42.fr>
2021-12-17 18:40:03 +0100

    gnl

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tbrebion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 17:02:38 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/12/17 17:04:29 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/12/17 18:39:14 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 
 char	*get_next_line(int fd);
 char	*get_line(char *str);
-
+char	*ft_strchr(const char *str, char c);
+char	*ft_strjoin(const char *s1, const char *s2);
+int			ft_strlen(char *str);
 # endif
--- Get_next_line/get_next_line_utils.c
+++ Get_next_line/get_next_line_utils.c
@@ -1,2 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbrebion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 17:11:53 by tbrebion          #+#    #+#             */
+/*   Updated: 2021/12/17 18:36:48 by tbrebion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return(i);
+}
+
+char	*ft_strchr(const char *str, char c)
+{
+	char	*s;
+
+	s = (char *)str;
+	while (*s != c)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return (s);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+	int	k;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	res = malloc(sizeof(char) * ft_strlen(s1) ft_strlen(s2) + 1);
+	if (res = NULL)
+		return (NULL);
+	while (s1[i])
+		res[k++] = s1[i++];
+	while (s2[j])
+		res[k++] = s2[j++];
+	res[k] = '\0';
+	free(s1);
+	return (res);
+}
