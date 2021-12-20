060699d86c496eb9dd12f499342b9187d65f3ec4
Titouan BREBION <tbrebion@e3r7p23.clusters.42paris.fr>
2021-12-20 14:44:54 +0100

    test are not good

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 12:42:30 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/12/17 18:51:07 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/12/20 14:43:38 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*get_line(char *str)
 		return (NULL);
 	while (str[i] && str[i] != '\n')
 		i++;
-	res = malloc(sizeof(char) * i + 1);
+	res = malloc(sizeof(char) * i + 2);
 	if (!res)
 		return (NULL);
 	i = 0;
@@ -33,11 +33,26 @@ char	*get_line(char *str)
 	}
 	if (str[i] == '\n')
 	{
-		res[i] == str[i];
+		res[i] = str[i];
 		i++;
 	}
 	res[i] = '\0';
 	return (res);
+}
+
+char	*get_save(int fd)
+{
+	char	*save;
+	int		size;
+
+	save = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!save)
+		return (NULL);
+	size = read(fd, save, BUFFER_SIZE);
+	if (size == -1)
+		return (NULL);
+	save[size] = '\0';
+	return (save);
 }
 
 char	*get_next_line(int fd)
@@ -47,5 +62,8 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	buff = get_save(fd);
+	res = get_line(buff);
+	free(buff);
 	return (res);
 }
--- Get_next_line/get_next_line_utils.c
+++ Get_next_line/get_next_line_utils.c
@@ -6,12 +6,12 @@
 /*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 17:11:53 by tbrebion          #+#    #+#             */
-/*   Updated: 2021/12/17 18:52:05 by tbrebion         ###   ########.fr       */
+/*   Updated: 2021/12/20 14:38:22 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+/*
 int	ft_strlen(char *str)
 {
 	int	i;
@@ -58,4 +58,4 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	res[k] = '\0';
 	free(s1);
 	return (res);
-}
+}*/
