cbc649e08f3ddf9f21ec90dc61311b8965b6206a
Titouan BREBION <tbrebion@e1r4p3.42.fr>
2022-01-17 11:09:02 -0800

    ok

--- push_swap/cmd/utils2.c
+++ push_swap/cmd/utils2.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 06:12:20 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/01/17 09:37:10 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/01/17 09:45:43 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,7 @@ void	rra(t_list **stack_a)
 	if (!(*stack_a) || ft_lstsize(*stack_a) < 2)
 		return ;
 	save = (*stack_a);
-	while ((*stack_a)->next->next != NULL)
-		(*stack_a)->next = (*stack_a);
-	tmp = (*stack_a)->next;
-	(*stack_a)->next = NULL;
-	(*stack_a) = tmp;
-	(*stack_a)->next = save;
+
 }
 /*
 void	rrb(t_list **stack_b)
