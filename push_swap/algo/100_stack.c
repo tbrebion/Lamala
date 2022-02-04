dfb82f5840f7b6872bb78e3591796311a59f7539
Titouan BREBION <tbrebion@e1r4p6.42.fr>
2022-02-04 13:45:58 +0100

    ok

--- push_swap/algo/100_stack.c
+++ push_swap/algo/100_stack.c
@@ -6,23 +6,24 @@
 /*   By: tbrebion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 18:17:35 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/02/04 11:04:13 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/02/04 13:27:02 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-/*void	sort_100_stack(t_list **stack_a, t_list **stack_b)
+void	sort_100_stack(t_list **stack_a, t_list **stack_b)
 {
 	int	i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	while (check_order(stack_a) != 1)
-	{
+	hundred_stack_utils(stack_a, stack_b);
+	while (i++ < 25)
+		pa(stack_a, stack_b);
+//	while (check_order(stack_a) != 1)
+//	{
 		hundred_stack_utils_1(stack_a, stack_b, i, j);
-		(*stack_a) = (*stack_a)->next;
-		(*stack_b) = (*stack_b)->next;
-	}
-}*/
+//	}
+}
