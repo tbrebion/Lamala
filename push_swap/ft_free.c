201d8835e653aac4a96f59d7794231a2b8d9e405
Titouan BREBION <tbrebion@e1r5p5.42.fr>
2022-02-08 17:57:38 +0100

    ok

--- push_swap/ft_free.c
+++ push_swap/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 15:52:46 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/02/08 15:58:31 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/02/08 17:31:16 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 void	free_stack(t_list *stack_a, t_list *stack_b)
 {
-	while (stack_a && stack_b)
+	t_list	*tmp;
+
+	while (stack_a != NULL)
 	{
+		tmp = stack_a->next;
 		free(stack_a);
-		free(stack_b);
-		stack_a = stack_a->next;
-		stack_b = stack_b->next;
+		stack_a = tmp;
 	}
-	return ;
+	while (stack_b != NULL)
+	{
+		tmp = stack_b->next;
+		free(stack_b);
+		stack_b = tmp;
+	}
 }
