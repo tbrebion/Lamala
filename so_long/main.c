071f4d3bae148a317a28f0f2c945047454f71c65
Titouan BREBION <tbrebion@e1r5p7.42.fr>
2022-02-15 19:48:11 +0100

    ok

--- so_long/main.c
+++ so_long/main.c
@@ -6,11 +6,12 @@
 /*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 11:31:52 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/02/11 11:38:45 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/02/15 19:46:15 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx/mlx.h"
+#include "minilibx_mms_20200219/mlx.h"
+#include <stdlib.h> 
 
 int	main(void)
 {
@@ -20,4 +21,5 @@ int	main(void)
 	mlx = mlx_init();		
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello");
 	mlx_loop(mlx);
+	return (0);
 }
