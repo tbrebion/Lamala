c463c5fcb2792962376fcd160f9bf7bc8f0b5828
Titouan BREBION <tbrebion@e1r3p2.42.fr>
2022-02-16 14:48:52 +0100

    ok

--- so_long/main.c
+++ so_long/main.c
@@ -6,20 +6,40 @@
 /*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 11:31:52 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/02/15 19:46:15 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/02/16 14:40:45 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx_mms_20200219/mlx.h"
+#include "mlx/mlx.h"
 #include <stdlib.h> 
+
+typedef	struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_data;
+
+void	my_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();		
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "HELLO");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
-	return (0);
 }
---
+++ so_long/mlx/README.md
@@ -0,0 +1,8 @@
+# MinilibX
+
+This is the MinilibX, a simple X-Window (X11R6) programming API
+in C, designed for students, suitable for X-beginners.
+
+## Licence
+
+Copyright Olivier CROUZET - 2014-01-06 -
