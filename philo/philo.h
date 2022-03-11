ffda6329deea4c5d3deedb45f7978ffff399d693
Titouan BREBION <tbrebion@e1r4p7.42.fr>
2022-03-11 11:33:40 +0100

    ...

---
+++ philo/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 11:26:53 by tbrebion          #+#    #+#             */
+/*   Updated: 2022/03/11 11:33:12 by tbrebion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include<string.h>
+# include<unistd.h>
+# include<stdlib.h>
+# include<stdio.h>
+# include<sys/time.h>
+# include<pthread.h>
+
+
+#endif
