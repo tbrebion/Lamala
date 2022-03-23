2f0f559e585e1bbcdb6047c13831afe5788434bd
Titouan BREBION <tbrebion@e1r5p7.42.fr>
2022-03-23 17:01:30 +0100

    ...

--- philo/routine.c
+++ philo/routine.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 15:02:20 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/03/23 15:29:52 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/03/23 17:01:10 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	eat_action(t_philo *philo)
 	print_things(data, philo->id, "is eating");
 	philo->t_last_meal = timestamp();
 	pthread_mutex_unlock(&(data->meal_check));
-	//sleep();  SUSPEND THREAD ?
+	wait_action(data->time_eat, data);
 	(philo->x_ate)++;
 	pthread_mutex_unlock(&(data->fork_m[philo->left_fork_id]));
 	pthread_mutex_unlock(&(data->fork_m[philo->right_fork_id]));
@@ -36,12 +36,5 @@ void	routine(t_philo *philo)
 	t_data	*data;
 
 	data = philo->data;
-	if((data->nb_philo % 2) == 0) //pair
-	{
-		
-	}
-	else //impair
-	{
 
-	}
 }
