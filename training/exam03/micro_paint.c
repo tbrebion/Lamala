0ceb638326c3b158391fd06e2299bd4f11babc4c
Titouan BREBION <tbrebion@e1r6p7.42.fr>
2022-03-30 13:52:41 +0200

    ...

--- training/exam03/micro_paint.c
+++ training/exam03/micro_paint.c
@@ -6,12 +6,30 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 11:26:55 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/03/29 12:06:48 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/03/30 11:54:23 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+typedef struct s_zone
+{
+	int		width;
+	int		height;
+	char	background;
+}	t_zone;
+
+typedef struct s_shape
+{
+	char			type;
+	float			x;
+	float			y;
+	float			width;
+	float			height;
+	char			color;
+	struct s_shape	*next;
+}	t_shape;
 
 
